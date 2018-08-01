2d7499f3a309f8b4580e0b19bd4b98bef6dd5951
Anthony Johnson <anthony.johnson@U30.com>
2018-08-01 15:18:45 -0400

    Updating partial and server.js to display projects page correctly

--- server.js
+++ server.js
@@ -74,11 +74,10 @@ app.get('/about', (req, res) => {
 
 app.get('/projects', (req, res) => {
     res.render('projects.hbs', {
-        projectList: {
             Project1: project1link,
             Project2: project2link,
             Project3: project3link
-        } 
+        
     });
 })
 
